559d3babdeafbe38beb052bc5e80307258c61cf0
Ahmed Hamzawy <96156858+Ahmedhamzawy96@users.noreply.github.com>
2022-08-04 07:00:22 +0200

    Update Program.cs

--- API and DataBase/Program.cs
+++ API and DataBase/Program.cs
@@ -4,12 +4,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+string ploicy = " ";
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<CompanyContext>(Options => Options.UseSqlServer("server=.;database=ElMokhtarDB;trusted_connection=true;"));
-
+builder.Services.AddCors(options => options.AddPolicy(ploicy, builder =>
+{
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+}));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -18,8 +23,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
 app.UseRouting();
+app.UseCors(ploicy);
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
