778a72ef87a13f5a608425c6520293107adc083b
Abdallah Maher <83484310+Maheer27@users.noreply.github.com>
2023-08-11 18:36:46 +0300

    PrintCustomerAccount

--- API and DataBase/Program.cs
+++ API and DataBase/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<CompanyContext>(Options => Options.UseSqlServer("Data Source=SQL8004.site4now.net;Initial Catalog=db_a8b9a3_elmokhtar;User Id=db_a8b9a3_elmokhtar_admin;Password=@A123456"));
-//builder.Services.AddDbContext<CompanyContext>(Options => Options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=db_a8b9a3_elmokhtar;integrated security=true;persist security info=false"));
+//builder.Services.AddDbContext<CompanyContext>(Options => Options.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=db_a8b9a3_elmokhtar;integrated security=true;persist security info=false"));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
 {
